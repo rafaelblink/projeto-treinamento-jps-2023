c78ca38aeee7fe8ad35553d26999cfb92f226403
Rafael Ferreira <rferreiraa@indracompany.com>
2023-10-28 09:28:20 -0300

    feat: cadastro de produtos funcional

--- src/app/pages/cadastrar-usuarios/cadastrar-usuarios.component.ts
+++ src/app/pages/cadastrar-usuarios/cadastrar-usuarios.component.ts
@@ -1,20 +1,37 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
-
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { IUsuario } from 'src/app/interfaces/usuario';
+import { UsuariosService } from 'src/app/services/usuarios.service';
+import Swal from 'sweetalert2';
 @Component({
   selector: 'app-cadastrar-usuarios',
   templateUrl: './cadastrar-usuarios.component.html',
-  styleUrls: ['./cadastrar-usuarios.component.css']
+  styleUrls: ['./cadastrar-usuarios.component.css'],
 })
 export class CadastrarUsuariosComponent {
+  constructor(private usuariosService: UsuariosService) {}
 
   usuarioForm = new FormGroup({
-    nome: new FormControl(''),
-    idade: new FormControl(0)
-  })
+    nome: new FormControl('', Validators.required),
+    idade: new FormControl(0),
+  });
 
   enviar() {
-    console.log(this.usuarioForm.value)
-  }
+    const usuario: IUsuario = this.usuarioForm.value as IUsuario;
+    usuario.ativo = true;
 
+    this.usuariosService.cadastrarUsuario(usuario).subscribe(
+      (result) => {
+        Swal.fire(
+          'PARABÉNS CHAMPS!!',
+          'Usuário cadastrado com sucesso!',
+          'success'
+        );
+      },
+      (error) => {
+        const { message } = error;
+        Swal.fire('DEU ERRO', message, 'error');
+      }
+    );
+  }
 }
--- src/app/services/usuarios.service.ts
+++ src/app/services/usuarios.service.ts
@@ -3,16 +3,18 @@ import { Injectable } from '@angular/core';
 import { IUsuario } from '../interfaces/usuario';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UsuariosService {
-
   api = 'http://localhost:3000/usuarios';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   buscarTodos() {
     return this.http.get<IUsuario[]>(this.api);
   }
 
+  cadastrarUsuario(usuario: IUsuario) {
+    return this.http.post<IUsuario>(this.api, usuario);
+  }
 }
